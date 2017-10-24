9fbeced43122cd82d361f58e63a7197792e3c6eb
Hiroaki Yutani <yutani.ini@gmail.com>
2017-10-25 01:05:09 +0900

    add cache

--- R/build.R
+++ R/build.R
@@ -1,21 +1,26 @@
-# passed from blogdown::build_site()
+# catch "local" arg passed from blogdown::build_site()
 local <- commandArgs(TRUE)[1] == "TRUE"
 
+# set common options ofr knitr
 knitr::opts_knit$set(
   base.dir = normalizePath("static/", mustWork = TRUE),
   base.url = "/"
 )
 
-rmds <- list.files("content", "[\\.]Rmd$", recursive = TRUE, full.names = TRUE)
+knitr::opts_chunk$set(
+  cache.path = normalizePath("cache/", mustWork = TRUE)
+)
 
-for (rmd in rmds) {
+# knit only ones that needs updated
+rmds <- list.files("content", "[\\.]Rmd$", recursive = TRUE, full.names = TRUE)
+mds <- sub("\\.Rmd$", ".md", rmds)
+already_knitted <- file.exists(mds) & utils::file_test("-ot", rmds, mds)
+
+message("Skip:\n\t", paste(rmds[already_knitted], collapse = "\n\t"))
+
+for (rmd in rmds[!already_knitted]) {
   wo_ext <- tools::file_path_sans_ext(rmd)
   md <- glue::glue("{wo_ext}.md")
-  
-  if (file.exists(md) && utils::file_test("-ot", rmd, md)) {
-    message(glue::glue("skip {rmd}"))
-    next
-  }
   
   knitr::opts_chunk$set(
     fig.path = glue::glue("post/{basename(wo_ext)}_files/figure-html/")
